6a35a86b4bba25927f5fa863f0ed62b5d2949f5a
Maulana Agung <iccecoffee@gmail.com>
2014-12-24 22:42:06 +0700

    Add animation to elements

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+(function($) {
+
+    var config = {
+
+        easing: 'hustle',
+        reset:  true,
+        delay:  'onload',
+        vFactor: 0.90
+    }
+
+    window.sr = new scrollReveal(config);
+
+})(jQuery);
